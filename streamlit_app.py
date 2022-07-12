f600d3b7d97bd3da20dc3abd30cafd6fb917ba89
randipchanantt <106165090+randipchanantt@users.noreply.github.com>
2022-07-12 14:33:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,14 @@
 import streamlit
 import pandas
 
+streamlit.header("Fruityvice Fruit Advice!")
+
+
+import requests
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
